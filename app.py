6fa01e774a19dd289a7980f69cc1536637851bf7
Allan ixart <157831738+ALL-ALL-ALL@users.noreply.github.com>
2025-10-01 00:54:44 +0000

    en cours..mise en place du  graphique a) demandé

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ figure = px.pie(données, values='qte', names='region', title='quantité vendue 
 figure.write_html('ventes-par-region.html')
 
 print('ventes-par-région.html généré avec succès !')
+
+
+# a. Premier graphique - les ventes par produit -prendre exemples en haut 
+# a. Premier graphique - les ventes par produit -prendre exemples en haut 
+
+données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+
+figure = px.pie(données, values='qte',names='produit',title='ventes par produit')
+
+figure.write_html('les-ventes-par-produit.html')
+
+print('les-ventes-par-produit.html cree avec succés')
